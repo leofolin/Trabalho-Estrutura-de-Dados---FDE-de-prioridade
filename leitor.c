670fa087cc92f3feaee271adf89f56d9bb094e1f
atomicpenguinz <heitorlcaetano@gmail.com>
2025-10-08 18:28:37 -0300

    arrumar ordem

--- leitor.c
+++ leitor.c
@@ -1,5 +1,13 @@
 #include "generica_FDE.c"
 
+#if RANDOM 
+void randomiza() {
+	system("sort -R" ARQUIVO "> Dataset2.csv");
+}
+#undef ARQUIVO
+#define ARQUIVO "Dataset2.csv"
+#endif
+
 bool gerarFila(desc *fila, int n) {
 	FILE *file = fopen(ARQUIVO, "r");
 	if(file == NULL) {
@@ -22,11 +30,3 @@ bool gerarFila(desc *fila, int n) {
 	}
 	return true;
 }
-
-#if RANDOM 
-void randomiza() {
-	system("sort -R" ARQUIVO "> Dataset2.csv");
-}
-#undef ARQUIVO
-#define ARQUIVO "Dataset2.csv"
-#endif
