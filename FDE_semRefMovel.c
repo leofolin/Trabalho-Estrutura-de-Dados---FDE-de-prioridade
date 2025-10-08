85549dbd7c4f80a2be300f584eb311c6e45b9b59
atomicpenguinz <heitorlcaetano@gmail.com>
2025-10-07 21:52:07 -0300

    funções reinicia e tamanhoDaFila

--- FDE_semRefMovel.c
+++ FDE_semRefMovel.c
@@ -10,6 +10,32 @@ desc *cria(int tam) {
 	return descricao;
 }
 
+int tamanhoDaFila(desc *p) {
+	struct nodo *aux = p->cauda;
+	int i = 0;
+	while(aux->defronte != NULL) {
+		aux = aux->defronte;
+		i++;
+	}
+	return i;
+}
+
+bool reinicia(desc *p) {
+	if(p->cauda == NULL || p->frente == NULL) {
+			return false;
+	}	
+	struct nodo *aux = p->cauda->defronte;
+	while(aux != NULL) {
+		free(p->cauda);
+		p->cauda = aux;
+		aux = aux->defronte;
+	}
+	free(p->cauda);
+	p->frente = NULL;
+	p->cauda = NULL;
+	return true;
+}
+
 bool removeFrente(desc *p, info *alvo) {
 	if(p->cauda == NULL || p->frente == NULL) {
 		return false;
