a708349eb699bdf2d3077319aeeee5185291db76
atomicpenguinz <heitorlcaetano@gmail.com>
2025-10-08 09:52:46 -0300

    Separação de funções usadas em ambas as implementações

---
+++ generica_FDE.h
@@ -0,0 +1,62 @@
+#include "arq.h"
+// Funções genéricas usadas nas duas implementações de fila
+
+desc *cria(int tam) {
+	desc *descricao = malloc(sizeof(desc));
+	if(descricao != NULL) {
+		descricao->cauda = NULL;
+		descricao->frente = NULL;
+		descricao->tamInfo = tam;
+	}
+	return descricao;
+}
+
+int tamanhoDaFila(desc *p) {
+	struct nodo *aux = p->cauda;
+	int i = 0;
+	while(aux->defronte != NULL) {
+		aux = aux->defronte;
+		i++;
+	}
+	return i;
+}
+
+bool reinicia(desc *p) {
+	if(testaVazia(p)) {
+		return false;
+	}	
+	struct nodo *aux = p->cauda->defronte;
+	while(aux != NULL) {
+		free(p->cauda);
+		p->cauda = aux;
+		aux = aux->defronte;
+	}
+	free(p->cauda);
+	p->frente = NULL;
+	p->cauda = NULL;
+	return true;
+}
+
+struct desc *destroi(desc *p) {
+	reinicia(p);
+	free(p);
+	return NULL;
+}
+bool buscaNaCauda(desc *p, info *alvo) {
+	if(testaVazia(p)) {
+		return false;
+	}
+	*alvo = p->cauda->dados;
+	return true;
+}
+
+bool buscaNaFrente(desc *p, info *alvo) {
+	if(testaVazia(p)) {
+		return false;
+	}
+	*alvo = p->frente->dados;
+	return true;
+}
+bool testaVazia(desc *p) {
+	return p->cauda == NULL || p->frente == NULL;
+}
