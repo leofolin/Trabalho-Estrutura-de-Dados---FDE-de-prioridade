9fbfc499b3371f71174d35cff029f6c53e60e36c
leofolin <leonardoy.kath@gmail.com>
2025-10-08 11:35:21 -0300

    Update FDE_refMovel.c

--- FDE_refMovel.c
+++ FDE_refMovel.c
@@ -1,5 +1,37 @@
 #include "generica_FDE.c"
 
-bool insere(info *novo, desc *p) {
-	
+bool insere(info *nodoInfo, desc *p){
+
+	bool ok = false;
+	struct nodo *novoNodo = NULL;
+	novoNodo = malloc(sizeof(struct nodo));
+	if(novoNodo == NULL) return false;
+	memcpy(&(novoNodo->dados), nodoInfo, p->tamInfo);
+
+	if(p->frente == NULL && p->cauda == NULL){
+		novoNodo->defronte = NULL;
+		novoNodo->atras = NULL;
+		p->frente = novoNodo;
+		p->cauda = novoNodo;
+		ok = true;
+	}
+	else {
+		if(novoNodo->dados.ranking < p->cauda->dados.ranking){
+			novoNodo->defronte = p->cauda;
+			novoNodo->atras = NULL;
+			p->cauda->atras = novoNodo;
+			p->cauda = novoNodo;
+			ok = true;
+		}
+		else if(novoNodo->dados.ranking > p->frente->dados.ranking){
+			novoNodo->atras = p->frente;
+			novoNodo->defronte = NULL;
+			p->frente->defronte = novoNodo;
+			p->frente = novoNodo;
+			ok = true;
+		}
+		else{
+
+		}
+	}
 }
