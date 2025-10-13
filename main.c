c794e7fc48709b4b44e2f5bb4344dc252df14a22
atomicpenguinz <heitorlcaetano@gmail.com>
2025-10-13 19:44:41 -0300

    Iniciar menu

--- main.c
+++ main.c
@@ -1,5 +1,43 @@
-#if REF_MOVEL
-#include "FDE_refMovel.c"
-#else 
-#include "FDE_semRefMovel.c"
-#endif
+#include "arq.h"
+
+int main() {
+    struct desc *fila = cria(sizeof(info));
+    unsigned int qtd = gerarFila(fila, NUMERO);
+    printf("Criada fila com %i elementos, com %u iterações", NUMERO, qtd);
+    int opcao = -1;
+    do {
+        menu();
+        scanf("%i", &opcao);
+        info dados;
+        switch(opcao) {
+        case(0):
+            printf("Saindo...");
+            break;
+        case(1):
+            removeFrente(fila, &dados);
+            printf("Nodo removido:");
+            printaNodo(dados);
+        case(2):
+            printf("Tamanho: %u", tamanhoDaFila(fila));
+            break;
+        case(3):
+            buscaNaCauda(fila, &dados);
+            printf("Nodo encontrado:");
+            printaNodo(dados);
+            break;
+        case(4):
+            buscaNaFrente(fila, &dados);
+            printf("Nodo encontrado:");
+            printaNodo(dados);
+        }
+    } while(opcao != 0);
+}
+
+void menu() {
+    printf("Escolha uma das opções:\n");
+    printf("0 - Sair\n"
+           "1 - Remover nodo da frente\n"
+           "2 - Mostrar tamanho da fila\n"
+           "3 - Mostrar elemento da cauda\n"
+           "4 - Mostrar elemento da frente\n");
+}
