396e0dbbd098450bd0452c07993f078a303ef935
atomicpenguinz <heitorlcaetano@gmail.com>
2025-10-13 12:53:58 -0300

    leitor.c agora é csv.c

---
+++ csv.c
@@ -0,0 +1,32 @@
+#include "arq.h"
+
+#if RANDOM
+void randomiza() {
+    system("sort -R " ARQUIVO "> Dataset2.csv");
+}
+#undef ARQUIVO
+#define ARQUIVO "Dataset2.csv"
+#endif
+
+unsigned int gerarFila(desc *fila, int n) {
+    FILE *file = fopen(ARQUIVO, "r");
+    if(file == NULL) {
+        printf("Arquivo %s não encontrado", ARQUIVO);
+        return 0;
+    }
+
+    info dados;
+    unsigned int soma = 0, qtd;
+    for(int i = 0; i < n; i++) {
+        fscanf(file, "%s,%i,%i,%s \n",  dados.nome,
+               &dados.matricula,
+               &dados.ranking,
+               dados.curso);
+        qtd = insere(&dados,fila);
+        if(!qtd) {
+            return 0;
+        }
+        soma += qtd;
+    }
+    return soma;
+}
