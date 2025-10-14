258541318a2a873571f119df08d007ac8c31f1b3
atomicpenguinz <heitorlcaetano@gmail.com>
2025-10-13 22:01:41 -0300

    usar regex para não corromper

--- csv.c
+++ csv.c
@@ -18,7 +18,7 @@ unsigned int gerarFila(desc *fila, int n) {
     info dados;
     unsigned int soma = 0, qtd;
     for(int i = 0; i < n; i++) {
-        fscanf(file, "%s,%i,%i,%s \n",  dados.nome,
+        fscanf(file, "%49[^,],%i,%i,%49[^\n]\n",  dados.nome,
                &dados.matricula,
                &dados.ranking,
                dados.curso);
