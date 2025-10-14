bf47ed93317515c40be995d690d84cd821f48061
atomicpenguinz <heitorlcaetano@gmail.com>
2025-10-14 08:54:57 -0300

    colocar todo o menu como uma função

--- main.c
+++ main.c
@@ -4,13 +4,20 @@ int main() {
     struct desc *fila = cria(sizeof(info));
     unsigned int qtd = gerarFila(fila, NUMERO);
     printf("Criada fila com %i elementos, com %u iterações\n", NUMERO, qtd);
+    menu();
+}
+void menu() {
     int opcao = -1;
     do {
-        menu();
-        scanf("%i", &opcao);
-        info dados;
+        printf("Escolha uma das opções:\n");
+        printf("0 - Sair\n"
+               "1 - Remover nodo da frente\n"
+               "2 - Mostrar tamanho da fila\n"
+               "3 - Mostrar elemento da cauda\n"
+               scanf("%i", &opcao);
+               info dados;
         switch(opcao) {
-        case(0):
+    case(0):
             printf("Saindo...");
             break;
         case(1):
@@ -34,13 +41,5 @@ int main() {
             printf("Opção inválida. Tente novamente");
         }
     } while(opcao != 0);
-}
-
-void menu() {
-    printf("Escolha uma das opções:\n");
-    printf("0 - Sair\n"
-           "1 - Remover nodo da frente\n"
-           "2 - Mostrar tamanho da fila\n"
-           "3 - Mostrar elemento da cauda\n"
-           "4 - Mostrar elemento da frente\n");
+}          "4 - Mostrar elemento da frente\n");
 }
